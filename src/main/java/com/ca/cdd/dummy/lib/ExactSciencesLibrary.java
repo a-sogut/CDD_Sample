0ef1495cdad49256a15e38774a0d3a2fa2228285
Uri Scheiner <uri.scheiner@broadcom.com>
2019-08-13 13:52:01 -0700

    Update ExactSciencesLibrary.java

--- src/main/java/com/ca/cdd/dummy/lib/ExactSciencesLibrary.java
+++ src/main/java/com/ca/cdd/dummy/lib/ExactSciencesLibrary.java
@@ -1,7 +1,7 @@
 package com.ca.cdd.dummy.lib;
 
 public class ExactSciencesLibrary extends Library {
-    private static String NAME = "ExactSciences";
+    private static String NAME = "ExactSciences1";
 
     @Override
     public String getName() {
