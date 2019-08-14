0c74e1336820ec4342000452b5464dd4be49ea87
Uri Scheiner <uri.scheiner@broadcom.com>
2019-08-14 13:46:27 -0700

    Update Payment.java

--- src/main/java/com/ca/cdd/dummy/lib/Payment.java
+++ src/main/java/com/ca/cdd/dummy/lib/Payment.java
@@ -1,7 +1,7 @@
 package com.ca.cdd.dummy.lib;
 
 public class Payment extends Library {
-    private static String NAME = "Comics";
+    private static String NAME = "Comics1";
     @Override
     public String getName() {
         return NAME;
