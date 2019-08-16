eb0564c8747d934a866074f451d54dba613d35ee
Uri Scheiner <uri.scheiner@broadcom.com>
2019-08-16 14:52:26 -0700

    Update LoginURLStrings.java

--- src/main/java/com/ca/cdd/dummy/lib/LoginURLStrings.java
+++ src/main/java/com/ca/cdd/dummy/lib/LoginURLStrings.java
@@ -37,5 +37,9 @@ public class LoginURLStrings extends Library implements InterfaceClass {
 	public String getLinkedInURL() {
         return "/login/linkedin";
     }
+	public String getGitHubURL() {
+        return "/login/github";
+    }
+
 	
 }
